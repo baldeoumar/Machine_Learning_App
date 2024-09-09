5c6cbebe528051b34f5fe926e33bb56b962f7541
baldeoumar <123509681+baldeoumar@users.noreply.github.com>
2024-09-09 10:50:01 +0000

    Add files via upload

---
+++ Logistique_maladie.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 15:04:20 2024
+
+@author: o.balde
+"""
+
+#from flask import Flask,json,request,jsonify
+import streamlit as st
+import pickle
+#app=Flask(__name__)
+model=pickle.load(open('logistique.pkl','rb'))
+
+def main():
+    st.title("Bienvenu de prediction de l'etat de sante Cardiaque des Patients")
+    # les entrees
+   # age	sex	cp	trestbps	chol	fbs	restecg	thalach	exang	oldpeak	slope	ca	thal
+    age=st.text_input("Donner l'age")
+    sex=st.text_input("Donner le sexe")
+    cp=st.text_input('Donner le type de douleurs toraxiques')
+    trestbps=st.text_input("Donner la pression artérielle au repos (en mm Hg à l'admission à l'hôpital)")
+    chol=st.text_input("Donner le taux de cholestérol sérique en mg/dl")
+    fbs=st.text_input("Le (glycémie à jeun > 120 mg/dl): (1 = vrai ; 0 = faux) ")
+    restecg=st.text_input('résultats électrocardiographiques au repos')
+    thalach=st.text_input("fréquence cardiaque maximale atteinte")
+    exang=st.text_input("angine de poitrine induite par l'exercice (1 = oui ; 0 = non)")
+    oldpeak=st.text_input("dépression ST induite par l'exercice par rapport au repos")
+    slope=st.text_input("la pente du segment ST de pointe à l'effort")
+    ca=st.text_input("nombre de vaisseaux principaux (0-3) colorés par flourosopie")
+    thal=st.text_input("Thal : 3 = normal ; 6 = défaut fixe ; 7 = défaut réversible")
+    # prediction
+    if st.button('predict'):
+        makeprediction=model.predict([[age,sex,cp,trestbps,	chol,fbs,restecg,thalach,exang,	oldpeak,slope,ca,thal]])
+        output=round(makeprediction[0])
+        if output==0:
+            "Le patient n'est pas Malade."
+        else :
+            "Le patient est Malade."
+        #st.success('You can by your ... for {}'.format(output))
+if __name__=='__main__':
+    main()
+    
+# @app.route('/')
+# def home():
+#     return 'Bienvenue sur le site de prediction des prix'
+# @app.route("/predict", methods=["GET"])
+# def predict():
+#     v1=request.args.get('valeur1')
+#     v2=request.args.get('valeur1')
+#     v3=request.args.get('valeur1')
+#     makeprediction=model.predict()
---
+++ RegressionModel_Prices.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 15:04:20 2024
+
+@author: o.balde
+"""
+
+#from flask import Flask,json,request,jsonify
+import streamlit as st
+import pickle
+#import base64 as b
+#app=Flask(__name__)
+# def setback(fichier):
+#     bin_str=b.get_base64(fichier)
+#     page_bg_img='''
+#     <style>
+#     .stApp{
+#         background-image:url("data:image/png;base64,%s");
+#         background-size:cover
+#         }
+#     </style>
+#     ''' % bin_str
+#     st.markdown(page_bg_img,unsafe_allow_html=True)
+
+model=pickle.load(open('randomforest.pkl','rb'))
+# fichier='signature.jpg'
+# setback(fichier)
+
+def main():
+    st.title("Bienvenu de prediction des prix des téléphones portables")
+    # les entrees
+   # 'Screen Size (inches)','RAM (GB)','Storage (GB)','Battery Capacity (mAh)','Camera Quality (MP)'
+    screen=st.text_input("Donner la taille de l'Ecran en (Inches)")
+    ram=st.text_input("Donner la capacite de la RAM (GB)")
+    storage=st.text_input("Donner la Capacite de stockage (GB)")
+    battery=st.text_input('Donner la capacite de la battery (mAh)')
+    camera=st.text_input("Donner la qualite du Camera en (MP)")
+    # prediction
+    if st.button('predict'):
+        makeprediction=model.predict([[screen,ram,storage,battery,camera]])
+        output=round(makeprediction[0],2)
+        # if output==0:
+        #     "Le patient n'est pas Malade."
+        # else :
+        #     "Le patient est Malade."
+        st.success('Vous pouvez vendre votre Telephone à {} $'.format(output))
+if __name__=='__main__':
+    main()
+    
+# @app.route('/')
+# def home():
+#     return 'Bienvenue sur le site de prediction des prix'
+# @app.route("/predict", methods=["GET"])
+# def predict():
+#     v1=request.args.get('valeur1')
+#     v2=request.args.get('valeur1')
+#     v3=request.args.get('valeur1')
+#     makeprediction=model.predict()
